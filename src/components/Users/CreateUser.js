b5037264b8bb996b6e7e079a7da9a3f0b37d8874
Olga Shalakhina <osshalakhina@gmail.com>
2022-06-10 08:30:18 +0300

    Update

---
+++ src/components/Users/CreateUser.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./CreateUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
+
+const CreateUser = (props) => {
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+  const [error, setError] = useState();
+
+  const createUserHandler = (event) => {
+    event.preventDefault();
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+      setError({
+        title: "Incorrect input!",
+        message: "These fields cannot be empty!",
+      });
+      return;
+    }
+    if (+inputAge < 1) {
+      setError({
+        title: "Incorrect age!",
+        message: "Please enter the correct age bigger than 0!",
+      });
+      return;
+    }
+    //console.log(inputName, inputAge);
+    props.onCreateUser(inputName, inputAge);
+    setInputName("");
+    setInputAge("");
+  };
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(false);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          onCloseModal={errorHandler}
+          title={ErrorEvent.title}
+          message={error.message}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={createUserHandler}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            onChange={nameChangeHandler}
+            value={inputName}
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={inputAge}
+          />
+          <Button type="submit">Add user</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default CreateUser;
